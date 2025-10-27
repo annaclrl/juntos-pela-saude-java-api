bd80d92c5effa01cb97dfba69b0c63fe31fd495d
Anna <annaclararussoluca@gmail.com>
2025-10-27 16:24:09 -0300

    feat: criando exception para violação de constraint

---
+++ src/main/java/br/com/fiap/exception/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,30 @@
+package br.com.fiap.exception;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+
+        String mensagem = exception.getConstraintViolations()
+                .stream()
+                .findFirst()
+                .map(ConstraintViolation::getMessage)
+                .orElse("Erro de validação");
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(mensagem)
+                .type(MediaType.TEXT_PLAIN)
+                .build();
+    }
+}
